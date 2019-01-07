7ad071b6acf54ec6765144647f7696bb27a90a75
Jim Yeh <jim@bitmark.com>
2019-01-07 15:28:08 +0800

    Add a function to get bitmark block number

--- client.go
+++ client.go
@@ -11,6 +11,18 @@ import (
 	"time"
 )
 
+type Block struct {
+	Number    int64
+	Hash      string
+	Owner     string
+	BitmarkId string
+}
+
+type BlockResponse struct {
+	Blocks []Block `json:"blocks"`
+	Msg    string  `json:"message"`
+}
+
 type Transaction struct {
 	Tx  json.RawMessage `json:"tx"`
 	Msg string          `json:"message"`
@@ -158,4 +170,32 @@ func (c Client) GetBitmark(bitmarkId string, pending, provenance bool) ([]byte, 
 		return nil, fmt.Errorf(bmk.Msg)
 	}
 	return bmk.Bitmark, nil
+}
+
+func (c Client) GetBlockNumber() (int64, error) {
+	u := *c.u
+	u.Path = fmt.Sprintf("/v1/blocks")
+
+	req, _ := http.NewRequest("GET", u.String(), nil)
+	resp, err := c.c.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	var blockResp BlockResponse
+	d := json.NewDecoder(resp.Body)
+	err = d.Decode(&blockResp)
+	if err != nil {
+		return 0, fmt.Errorf("error when parsing request body: %s", err.Error())
+	}
+	if resp.StatusCode != 200 {
+		return 0, fmt.Errorf(blockResp.Msg)
+	}
+
+	if len(blockResp.Blocks) == 0 {
+		return 0, fmt.Errorf("invalid blocks return")
+	}
+
+	return blockResp.Blocks[0].Number, nil
 }
